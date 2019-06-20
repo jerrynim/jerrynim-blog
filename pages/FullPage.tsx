0c02aad4207e613e53e996f5dbd3f554389b8b53
jerrynim <tjerry3@naver.com>
2019-06-21 04:04:04 +0900

    css가 적용안된 view가 나오는 문제 해결

--- pages/FullPage.tsx
+++ pages/FullPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ReactFullpage from "@fullpage/react-fullpage";
 import Profile from "../components/Profile";
@@ -10,44 +10,50 @@ const Section = styled.div`
   height: 100%;
 `;
 const FullPage: React.FunctionComponent = () => {
+  //header link가 불러오기까지 기다린다.(css가 적용되기전에 200ms 정도 보여지는 문제)
+  const [loading, setLoading] = useState("default");
+  useEffect(() => {}, []);
   return (
-    <ReactFullpage
-      render={({ state, fullpageApi }) => {
-        console.log(state, fullpageApi);
-        return (
-          <>
-            <Helmet>
-              <link
-                href="https://fonts.googleapis.com/css?family=Press+Start+2P"
-                rel="stylesheet"
-              />
-              <link
-                href="https://unpkg.com/nes.css/css/nes.css"
-                rel="stylesheet"
-              />
-              <title>Jerrynim</title>
-            </Helmet>
-            <ReactFullpage.Wrapper>
-              <div className="section">
-                <Section>
-                  <Profile />
-                </Section>
-              </div>
-              <div className="section">
-                <Section>
-                  <Tech />
-                </Section>
-              </div>
-              <div className="section">
-                <Section>
-                  <Experience />
-                </Section>
-              </div>
-            </ReactFullpage.Wrapper>
-          </>
-        );
-      }}
-    />
+    <>
+      <Helmet
+        onChangeClientState={() => {
+          //link가 다 불러져왔을때 handle
+          setLoading("loaded");
+        }}
+      >
+        <link
+          href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+          rel="stylesheet"
+        />
+        <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+        <title>Jerrynim</title>
+      </Helmet>
+      {loading === "loaded" && (
+        <ReactFullpage
+          render={() => {
+            return (
+              <ReactFullpage.Wrapper>
+                <div className="section">
+                  <Section>
+                    <Profile />
+                  </Section>
+                </div>
+                <div className="section">
+                  <Section>
+                    <Tech />
+                  </Section>
+                </div>
+                <div className="section">
+                  <Section>
+                    <Experience />
+                  </Section>
+                </div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        />
+      )}
+    </>
   );
 };
 
