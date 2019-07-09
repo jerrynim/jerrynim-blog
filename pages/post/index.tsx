82953e2a6d89190f8f8f619e70e44e56c1ca5116
jerrynim <tjerry3@naver.com>
2019-07-09 15:53:23 +0900

    next 9.0

---
+++ pages/post/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import Link from "next/link";
+export default class extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello Next.js</p>
+      </div>
+    );
+  }
+}
