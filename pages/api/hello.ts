a4439c14ace5cab5600e40ef8b3035cae6ed40a3
jerrynim <tjerry3@naver.com>
2019-07-10 15:56:07 +0900

    remove api

--- pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next-server/dist/lib/utils";
-
-export default function handle(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req);
-  res.end("Hello World");
-}
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default () => {
-  const [data, setData] = useState("");
-
-  const getData = async () => {
-    const data = await fetch("http://localhost:3000/api/hello").then((r) =>
-      r.text()
-    );
-    console.log(data);
-    setData(data);
-  };
   return (
     <div>
       <p>Hello Next.jss</p>
-      <button onClick={getData}>asdf</button>
-      <br />
-      {data}
     </div>
   );
 };
