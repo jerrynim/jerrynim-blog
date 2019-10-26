bbf0575260424369b0187d6e265f5863ff5ec655
changju <tjerry3@naver.com>
2019-10-26 22:51:07 +0900

    Add top box ui

---
+++ components/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 50px;
+  height: 50px;
+`;
+
+const ScrollToTop: React.FC = () => {
+  return <Container>Top</Container>;
+};
+
+export default ScrollToTop;
